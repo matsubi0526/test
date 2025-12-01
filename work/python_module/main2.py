e9efa22cd7b1b07ea4f41a2e5241dfc5c5bf6238
MATSUBA Fumitaka <matsuba@gfd-dennou.org>
2025-12-01 19:35:30 +0900

    git add test scripts of importing python modules

---
+++ work/python_module/main2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# 2025-12-01
+# created by MATSUBA Fumitaka
+
+import os
+import sys
+import numpy as np
+
+from my_module import my_function
+
+def main():
+    print("this is a test")
+    my_function.test1()
+    print(np.e)
+
+if __name__ == "__main__":
+    main()
---
+++ work/python_module/my_module/my_function.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np_mod
+
+def main():
+    print("this is a my_function.py test")
+
+def test1():
+    print("this is a test1 method of my_function.py")
+    print(np_mod.e)
+
+if __name__ == "__main__":
+    main()
